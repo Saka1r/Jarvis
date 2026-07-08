57d4a3efb082eeeaac3db9df471281f5cae8181d
Saka1r <sakairlinux@gmail.com>
2026-07-08 13:32:08 +0300

    Add new subsystem

---
+++ subsystems/web_parser.py
@@ -0,0 +1,15 @@
+# subsystems/web_parser.py
+
+from urllib.request import urlopen
+
+class WebParser():
+    def __init__(self):
+        pass
+
+    def get_targets(self):
+        pass
+
+    def parsing(self, target):
+        pass
+
+    
