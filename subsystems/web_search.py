f231b4d67f6bd122425783627222f873805b22dd
Saka1r <sakairlinux@gmail.com>
2026-09-23 21:46:13 +0300

    rename

---
+++ subsystems/web_search.py
@@ -0,0 +1,16 @@
+# subsystems/web_search.py
+
+from urllib.request import urlopen
+
+class WebParser():
+    def __init__(self):
+        pass
+
+    def get_targets(self):
+        # This method should fetch and return the list of URLs that will be parsed
+        pass
+
+    def parsing(self, target):
+        # This method should parse the content of the target URL and return useful information
+        pass
+
